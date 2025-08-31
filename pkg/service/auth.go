f6a219868130a17fe182c97c047ab46d83ea6681
tiukalov@realx.tech <tiukalov@realx.tech>
2025-08-31 21:20:58 +0300

    lesson_7

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,19 @@ import (
 	"awesomeProject/pkg/repository"
 	"crypto/sha1"
 	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"strconv"
+	"time"
 )
 
 const salt = "asddfhghkjvghhfdgdfg"
+const signInKey = "stone_best"
+const tokenTTL = 12 * time.Hour
+
+type tokenClaims struct {
+	jwt.RegisteredClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +31,24 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(tokenTTL)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   strconv.Itoa(user.Id),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signInKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
