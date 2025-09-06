95928d3773e7ecf533d2d559ef0fb3b80216a569
tiukalov@realx.tech <tiukalov@realx.tech>
2025-09-07 00:17:44 +0300

    _2

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
@@ -14,41 +13,41 @@ const (
 )
 
 func (h *Handler) userIdentity(c *gin.Context) {
-	header := c.Request.Header.Get(authorizationHeader)
-
+	header := c.GetHeader(authorizationHeader)
 	if header == "" {
-		newErrorResponse(c, http.StatusUnauthorized, "empty authorization header")
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
 		return
 	}
 
 	headerParts := strings.Split(header, " ")
-	if len(headerParts) != 2 {
-		newErrorResponse(c, http.StatusUnauthorized, "invalid authorization header")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	if len(headerParts[1]) == 0 {
+		newErrorResponse(c, http.StatusUnauthorized, "token is empty")
 		return
 	}
 
 	userId, err := h.services.Authorization.ParseToken(headerParts[1])
-
 	if err != nil {
-		newErrorResponse(c, http.StatusUnauthorized, "invalid authorization header")
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
 		return
 	}
 
-	c.Set(userCtx, strconv.Itoa(userId))
+	c.Set(userCtx, userId)
 }
 
 func getUserId(c *gin.Context) (int, error) {
 	id, ok := c.Get(userCtx)
-
 	if !ok {
-		newErrorResponse(c, http.StatusInternalServerError, "invalid userId")
-		return 0, errors.New("invalid userId")
+		return 0, errors.New("user id not found")
 	}
 
 	idInt, ok := id.(int)
 	if !ok {
-		newErrorResponse(c, http.StatusInternalServerError, "invalid _userId")
-		return 0, errors.New("userId is not an integer")
+		return 0, errors.New("user id is of invalid type")
 	}
 
 	return idInt, nil
