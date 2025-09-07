bffeb1eea6083f0bc47ff94459116d818fba12de
tiukalov@realx.tech <tiukalov@realx.tech>
2025-09-07 22:53:54 +0300

    9_2

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -29,8 +29,26 @@ func (h *Handler) createList(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]interface{}{"id": id})
 }
 
+type getAllListsResponse struct {
+	Data []todo.TodoList `json:"data"`
+}
+
 func (h *Handler) getAllLists(c *gin.Context) {
-	c.String(200, "Все дошло")
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	lists, err := h.services.TodoList.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
 }
 
 func (h *Handler) getListById(c *gin.Context) {
